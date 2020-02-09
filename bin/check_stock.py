697f265b24146e4bad9df5fc54b9dce0a98492d0
Mike Vosskuhler <mike@localhost.localdomain>
2020-02-09 00:37:02 +0000

    updated check_inventory function

--- bin/check_stock.py
+++ bin/check_stock.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
-browser = webdriver.Chrome('/Users/mikevosskuhler/Downloads/chromedriver')
-
-browser.get("<producturl>")
-browser.find_element_by_xpath('//*[@id="9200000095000966"]/span[3]').click()
-browser.get('<basketurl>')
-browser.find_element_by_xpath('//*[@id="tst_quantity_dropdown"]').click()
-browser.find_element_by_xpath('//*[@id="tst_quantity_dropdown"]/option[11]').click()
-browser.find_element_by_xpath('//*[@id="mainContent"]/div[3]/div[1]/div/div[3]/div/div/div[1]/div/form/fieldset/div[2]/div[2]/input').send_keys('500')
-browser.find_element_by_xpath('//*[@id="mainContent"]/div[3]/div[1]/div/div[3]/div/div/div[1]/div/form/fieldset/div[2]/div[2]/div[2]/a').click()
+import time
+def check_inventory(producturl, basketurl, webdriver_location):
+    browser = webdriver.Chrome(webdriver_location)
+    browser.get(producturl)
+    browser.find_element_by_class_name('js_preventable_buy_action').click()
+    time.sleep(5)
+    browser.get(basketurl)
+    browser.find_element_by_id('tst_quantity_dropdown').click()
+    browser.find_element_by_xpath('//*[@id="tst_quantity_dropdown"]/option[11]').click()
+    browser.find_element_by_xpath('//*[@id="mainContent"]/div[3]/div[1]/div/div[3]/div/div/div[1]/div/form/fieldset/div[2]/div[2]/input').send_keys('500')
+    browser.find_element_by_xpath('//*[@id="mainContent"]/div[3]/div[1]/div/div[3]/div/div/div[1]/div/form/fieldset/div[2]/div[2]/div[2]/a').click()
+    return browser.page_source
